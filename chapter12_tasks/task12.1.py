1c73a7fb379cc50cce48914a59360f2aa89450c1
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-01-10 21:01:49 +0100

    task12.1 v1.0 ,adding task details for final version

--- chapter12_tasks/task12.1.py
+++ chapter12_tasks/task12.1.py
@@ -1,6 +1,18 @@
+"""
+Task 12.1
+Create a ping_ip_addresses function that checks if IP addresses are pingable.
+The function expects a list of IP addresses as an argument.
+The function must return a tuple with two lists:
+* list of available IP addresses
+* list of unavailable IP addresses
+To check the availability of an IP address, use the ping command.
+Restriction: All tasks must be done using the topics covered in this and previous chapters.
+"""
+
+
 import subprocess
 
-list_of_ip_addresses = [ "11.1.1.1", "2.2.2.2", "3.3.33.3", "8.8.8.8", "4.2.2.2" ]
+list_of_ip_addresses = [ "11.1.1.1", "2.2.2.2", "3.3.33.3", "8.8.8.8", "4.2.2.2", "9.9.9.9", "5.5.5.5", "3.1.1.1" ]
 
 reachable = [ ]
 unreachable = [ ]
