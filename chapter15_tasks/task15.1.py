715bd7ac5f47b35ae4a9679608d4013f3bbc9af7
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-01-21 13:14:49 +0100

    task15.1 with 3 solutions

--- chapter15_tasks/task15.1.py
+++ chapter15_tasks/task15.1.py
@@ -20,21 +20,42 @@ since the command output from network device is processed, not user input.
 import re
 from pprint import pprint
 
-result = [ ]
-regex = "ip address (\S+) (\S+)"
+# result = [ ]
+# regex = r"ip address (\S+) (\S+)"
+#
+#
+# def get_ip_from_cfg(device_config):
+#     with open(device_config) as f:
+#         for line in f:
+#             match_ip = re.search(regex, line)
+#             if match_ip:
+#                 result.append(match_ip.groups())
+#     return result
+#
+#
+# pprint(get_ip_from_cfg("config_r1.txt"))
 
+# second version
+result = [ ]
+
+
+# def get_ip_from_cfg(device_config):
+#     regex = r"ip address (\S+) (\S+)"
+#     with open(device_config) as k:
+#         for i in re.finditer(regex, k.read()):
+#             result.append(i.groups())
+#     return result
+#
+#
+# pprint(get_ip_from_cfg("config_r1.txt"))
+
+# third version
 
 def get_ip_from_cfg(device_config):
-    with open(device_config) as f:
-        for line in f:
-            match_ip = re.search(regex, line)
-            # match_ip1 = re.finditer(regex, line)
-            # print(match_ip1)
-            if match_ip:
-                result.append(match_ip.groups())
-    return result
+    regex = r"ip address (\S+) (\S+)"
+    with open(device_config) as k:
+        result_list = [ j.groups() for j in re.finditer(regex, k.read()) ]
+    return result_list
 
 
 pprint(get_ip_from_cfg("config_r1.txt"))
-
-# result = [ m.groups() for m in re.finditer(regex, f.read()) ]
