a40940ef1a5eeb802e1fb3854c37b825435a0fa0
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-01-21 10:20:59 +0100

    chapter15_general add search method

--- chapter15_tasks/re.search.py
+++ chapter15_tasks/re.search.py
@@ -1,8 +1,16 @@
+"""
+The re.search() method takes two arguments: a pattern and a string. The method looks for the first location where
+the RegEx pattern produces a match with the string.
+If the search is successful, re.search() returns a match object; if not, it returns None.
+"""
+
 import re
 
 string = "ASython Xython is fun ,Xython is fun to learn,;et's learn python fas "
 # check if 'Python' is at the beginning
 match = re.search('[Pp]ython', string)
+
+
 
 if match:
     print("pattern found inside the string")
