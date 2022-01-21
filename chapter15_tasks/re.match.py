01e63344b89b591be497e37d154718856611a984
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-01-21 10:43:04 +0100

    chapter15_general add search re.match

---
+++ chapter15_tasks/re.match.py
@@ -0,0 +1,12 @@
+import re
+
+target_string = "Emmais a basketball Emma player \n who was born on June 17, 1993"
+
+result = re.match(r"\w{4}", target_string)
+
+print(result.group())
+print(type(result))
+
+
+# Match is used to match only for beginning of string ,if there is no match it will return None
+# in case of match it will return Match object
