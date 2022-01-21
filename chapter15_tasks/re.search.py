c343e1a8cac0a8e01d863d7c79d155184befe384
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-01-21 10:25:15 +0100

    chapter15_general add search match_group

--- chapter15_tasks/re.search.py
+++ chapter15_tasks/re.search.py
@@ -8,7 +8,7 @@ import re
 
 string = "ASython Xython is fun ,Xython is fun to learn,;et's learn python fas "
 # check if 'Python' is at the beginning
-match = re.search('[Pp]ython', string)
+match = re.search('[Pp]ython (\w)', string)
 
 
 
@@ -19,3 +19,4 @@ else:
     print("pattern not found")
 
 # Search will try to find 1st match ,next outputs are ignored.
+
