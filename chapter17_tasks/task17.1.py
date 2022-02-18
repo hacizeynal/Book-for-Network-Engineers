a85ba47fd15b2133e7f763f2122a54155bab19b7
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-02-18 02:07:33 +0100

    task17.1 v1.1

--- chapter17_tasks/task17.1.py
+++ chapter17_tasks/task17.1.py
@@ -45,6 +45,7 @@ def write_dhcp_snooping_to_csv(configfile_from_device, csv_output):
         hostname = re.search("dhcp_snooping_([^/]+).txt", configfile_from_device)  # grab hostname from filename
         hostname = hostname.group(1)
         for i in catch_from_file:
+            print((hostname,)+i)
             write_data.writerow((hostname,) + i)
 
 
