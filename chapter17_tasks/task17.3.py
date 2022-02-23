b840a406e2cb17013ca66fb046ec0cdc566c1200
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-02-23 01:35:50 +0100

    task17.3 v1.0

--- chapter17_tasks/task17.3.py
+++ chapter17_tasks/task17.3.py
@@ -16,3 +16,33 @@ The function should return a dictionary like this:
 Interfaces must be written with a space. That is, so Fa 0/0, and not so Fa0/0.
 Check the function on the contents of the sh_cdp_n_sw1.txt file
 """
+import re
+from pprint import pprint
+
+regex_match = re.compile(
+    r"(?P<remote_device>\w+)  +(?P<local_interface>\S+ \S+)"
+    r"  +\d+  +[\w ]+  +\S+ +(?P<remote_interface>\S+ \S+)"
+)
+
+hostname_regex = re.compile(r"(?P<hostname>\S+)>show cdp neighbors")
+final_dictionary = {}
+
+
+def parse_sh_cdp_neighbors(cdp_neighbor_txt):
+    with open(cdp_neighbor_txt) as k:
+        for line in k:
+            hostname_match = hostname_regex.match(line)
+            details_match = regex_match.search(line)
+            if hostname_match:
+                hostname = hostname_match.group("hostname")
+                final_dictionary [ hostname ] = {}
+            elif details_match:
+                remote_device = details_match.group("remote_device")
+                local_interface = details_match.group("local_interface")
+                remote_interface = details_match.group("remote_interface")
+                final_dictionary [ hostname ] [ local_interface ] = {remote_device: remote_interface}
+        return final_dictionary
+
+
+if __name__ == "__main__":
+    pprint(parse_sh_cdp_neighbors("sh_cdp_sw1.txt"))
