879c5188f3c8551dfc659b3aeb1a1fc283b510c8
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-03-03 00:37:40 +0100

    paramiko v1.1

--- general /chapter18/netmiko_ssh.py
+++ general /chapter18/netmiko_ssh.py
@@ -6,9 +6,10 @@ from netmiko import (
     NetmikoAuthenticationException,
 )
 
+result = {}
+
 
 def send_show_command(device, commands):
-    result = {}
     try:
         with ConnectHandler(**device) as ssh:  # dictionary unpacking
             ssh.enable()
@@ -23,7 +24,7 @@ def send_show_command(device, commands):
 if __name__ == "__main__":
     with open("devices.yaml") as i:
         devices = yaml.safe_load(i)
-        print(type(devices))
+        # print(type(devices))
     for device in devices:
-        result = send_show_command(device, [ "sh hostname", "show version" ])
+        result = send_show_command(device, [ "sh clock", "show version" ])
         pprint(result, width=120)
