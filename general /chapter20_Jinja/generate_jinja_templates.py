610d15ddf7c8474a684449c286356ff8b1b2c8b2
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-03-09 20:15:30 +0100

    Jinja2 templates

--- general /chapter20_Jinja/generate_jinja_templates.py
+++ general /chapter20_Jinja/generate_jinja_templates.py
@@ -5,13 +5,14 @@ import os
 
 # python generate_jinja_templates.py templates/for.txt data_files/for.yml
 
-template_dir, template = os.path.split(sys.argv [ 1 ])
+template_directory, template_file = os.path.split(sys.argv [ 1 ])
 # assigned to template and templates will be assigned to template_dir
 
 vars_file = sys.argv [ 2 ]  # means third element in list
+print(vars_file)
 
 env = Environment(
-    loader=FileSystemLoader(template_dir),
+    loader=FileSystemLoader(template_directory),
     trim_blocks=True,
     lstrip_blocks=True)
 
@@ -19,6 +20,6 @@ template = env.get_template(template_file)
 
 with open(vars_file) as f:
     vars_dict = yaml.safe_load(f)
+    print(vars_dict)
 
-
-print(template.render(vars_dict))
+print(template.render(vars_dict))  # render will take * args or **kwargs as argument
