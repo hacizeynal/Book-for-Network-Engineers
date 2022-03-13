e8a4f029e143e30daeb10d7879fe51f5a93f25be
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-03-13 23:51:00 +0100

    task 20.1 v1.1

--- chapter20_tasks/task20.1.py
+++ chapter20_tasks/task20.1.py
@@ -1,16 +1,27 @@
+"""
+Task 20.1
+Create generate_config function.
+Function parameters:
+* template - path to the template file (for example, "templates/for.txt")
+* data_dict - a dictionary with values to be substituted into the template
+The function should return the generated configuration string.
+Check the operation of the function on the templates/for.txt template
+and data from the data_files/for.yml file.
+"""
+
 import yaml
 from jinja2 import Environment, FileSystemLoader
 import os
 
 
 def generate_config(template, input_dict):
-    template_directory, template_itself = os.path.split(template)
+    template_directory, main_template = os.path.split(template)
     env = Environment(
         loader=FileSystemLoader(template_directory),
         trim_blocks=True,
         lstrip_blocks=True)
-    k = env.get_template(template_itself)
-    return k.render(input_dict)
+    final_config = env.get_template(main_template)
+    return final_config.render(input_dict)
 
 
 if __name__ == "__main__":
