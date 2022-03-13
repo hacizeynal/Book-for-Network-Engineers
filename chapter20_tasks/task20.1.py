930bf42d1d846e8e7eb64130c8dae5b205e272d3
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-03-13 23:48:12 +0100

    task 20.1 v1.0

--- chapter20_tasks/task20.1.py
+++ chapter20_tasks/task20.1.py
@@ -1,8 +1,16 @@
 import yaml
+from jinja2 import Environment, FileSystemLoader
+import os
 
 
-def generate_config(template, input):
-    return None
+def generate_config(template, input_dict):
+    template_directory, template_itself = os.path.split(template)
+    env = Environment(
+        loader=FileSystemLoader(template_directory),
+        trim_blocks=True,
+        lstrip_blocks=True)
+    k = env.get_template(template_itself)
+    return k.render(input_dict)
 
 
 if __name__ == "__main__":
@@ -10,5 +18,4 @@ if __name__ == "__main__":
     template_file = "templates/for.txt"
     with open(data_file) as f:
         data = yaml.safe_load(f)
-        print(data)
     print(generate_config(template_file, data))
