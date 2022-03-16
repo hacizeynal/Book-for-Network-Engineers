f640d2eec911525faaebc9c40cdb4dc7bae354de
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-03-16 01:44:54 +0100

    task 20.4 v1.0

--- chapter20_tasks/t.py
+++ chapter20_tasks/t.py
@@ -7,9 +7,6 @@ a = [{'name': 'Fa0/1', 'ip': '10.255.0.1', 'area': 0, 'passive': False}, {'name'
      {'name': 'Fa0/1.200', 'ip': '10.255.2.1', 'area': 0, 'passive': False}, {'name': 'Fa0/0.10', 'ip': '10.0.10.1', 'area': 2, 'passive': True},
      {'name': 'Fa0/0.20', 'ip': '10.0.20.1', 'area': 2, 'passive': True}]
 
-k = {'Fa0/1': {'action': 'add', 'vlans': '10,20'},
+k = {'trunks': {'Fa0/1': {'action': 'add', 'vlans': '10,20'},
             'Fa0/2': {'action': 'only', 'vlans': '10,30'},
-            'Fa0/3': {'action': 'delete', 'vlans': 10}}
-
-for i in k:
-    print(i)
+            'Fa0/3': {'action': 'delete', 'vlans': 10}}}
