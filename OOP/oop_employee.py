30be3090248894349c9558bc84a958fa32f2d89a
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-03-24 23:33:10 +0100

    working with class attributes

--- OOP/oop_employee.py
+++ OOP/oop_employee.py
@@ -2,7 +2,8 @@
 
 class Employee:  # class is like an object constructor, or a "blueprint" for creating objects.
 
-    raise_amount = 1.15  # class attribute
+    number_of_employees = 0
+    raise_amount = 1.2  # class attribute
 
     def __init__(self, first, last, pay,
                  was_born):  # The __init__() function/method is called automatically every time the
@@ -12,6 +13,7 @@ class Employee:  # class is like an object constructor, or a "blueprint" for cre
         self.pay = pay  # attribute of class
         self.email = first.lower() + "." + last.lower() + "@ordubad.com"  # attribute of class
         self.was_born = was_born  # attribute of class
+        Employee.number_of_employees = Employee.number_of_employees + 1
 
     def display_full_name(self):  # this is method/function ,by default self is always taken
         return "{} {}".format(self.first, self.last)
@@ -23,26 +25,8 @@ class Employee:  # class is like an object constructor, or a "blueprint" for cre
 
 employee1 = Employee("Zeynal", "Hajili", 1000, "Ordubad")  # init method will run automatically to create employee1
 # object ,employee1 will be passed as self to the function and arguments will be taken automatically
-employee2 = Employee("Mamed", "Hajiyev", 1300, "Ordubad")
-
-# print(employee2.first)
-# print(employee1.email)
-# print(employee1.display_full_name())
-# print(Employee.display_full_name(employee2))
-
-# print(employee1.pay,"<-- Old salary for Employee1")
-# employee1.apply_raise()
-# print(employee1.pay,"<-- New salary for Employee1")
-# print(employee2.pay,"<-- Old salary for Employee2")
-# employee2.apply_raise()
-# print(employee2.pay,"<-- New salary for Employee2")
-# print(employee2.__dict__)
-# print(Employee.__dict__)
-
-# Employee.raise_amount = 15
-employee2.raise_amount = 19
-employee1.raise_amount = 2
-print(employee2.apply_raise())
-print(employee2.pay)
-print(employee1.apply_raise())
-print(employee1.pay)
+employee2 = Employee("Mamed", "Hajiyev", 2000, "Ordubad")
+employee3 = Employee("Haji", "Hajiyev", 200, "Julfa")
+employee4 = Employee("Huseyn", "Hajiyev", 999, "Baku")
+employee5 = Employee("Bagi", "Mammadov", 0, "Baku")
+print(Employee.number_of_employees)
