9442a92d6603612dd6be9f59e035cd3b57c6793e
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-03-30 00:19:25 +0200

    working with class attributes add # instance variable add # class variable add # class method

--- OOP/oop_employee.py
+++ OOP/oop_employee.py
@@ -18,17 +18,22 @@ class Employee:  # class is like an object constructor, or a "blueprint" for cre
         self.was_born = was_born  # attribute of class
         Employee.number_of_employees = Employee.number_of_employees + 1
 
-    def display_full_name(self):  # this is method/function ,by default self is always taken
+    def write_email_addresses(self):
+        with open("email_addresses.txt","w") as write_to_txt:
+            write_to_txt.write(self.email)
+
+    def display_full_name(self):  # this is regular method/function ,by default self is always taken as first argument
         return "{} {}".format(self.first, self.last)
 
     def apply_raise(self):
         # self.pay = int(self.pay * Employee.raise_amount)
-        self.pay = int(self.pay * self.raise_amount)
-
-    @classmethod  # class method
-    def set_raise_amount(cls, amount):
-        cls.raise_amount = amount
-
+        # self.pay = int(self.pay * self.raise_amount)
+        self.pay = int(self.pay * Employee.raise_amount)
+    #
+    # @classmethod  # class method
+    # def set_raise_amount(cls, amount):
+    #     cls.raise_amount = amount
+    # # @classmethod
 
 # print(Employee.number_of_employees)
 #
@@ -38,16 +43,21 @@ employee2 = Employee("Mamed", "Hajiyev", 2000, "Ordubad")
 employee3 = Employee("Haji", "Hajiyev", 0, "Julfa")
 employee4 = Employee("Huseyn", "Hajiyev", 999, "Baku")
 employee5 = Employee("Bagi", "Mammadov", 0, "Baku")
-employee5.raise_amount = 1.3
-# employee5.was_born = "Nakchivan"  # instance variable
-# employee4.pay = 350  # instance variable
-# print(employee4.__dict__)
-# print(Employee.number_of_employees)
-# print(employee5.was_born)
-Employee.set_raise_amount(10)
-print(Employee.raise_amount)
-print(employee5.raise_amount)
-print(employee3.raise_amount)
-print(employee1.raise_amount)
-print(employee2.raise_amount)
-print(employee4.raise_amount)
+# employee5.raise_amount = 1.3
+# employee5.set_raise_amount(11)
+# # employee5.was_born = "Nakchivan"  # instance variable
+# # employee4.pay = 350  # instance variable
+# # print(employee4.__dict__)
+# # print(Employee.number_of_employees)
+# # print(employee5.was_born)
+# Employee.set_raise_amount(10)
+# print(Employee.raise_amount)
+# print(employee5.raise_amount)
+# print(employee4.raise_amount)
+#
+# emp6_string = "Mahmud-Mahmudov-899-Cuba"
+# emp7_string = "Kubra-Mahmudova-99-Ganja"
+# name, last_name, pay,place = emp6_string.split("-")
+# employee6 = Employee(name,last_name,pay,place)
+
+Employee.write_email_addresses(employee5)
